622bdb00f52f8827443e014b284c1bf66215018f
Atridas87 <Atridas87@localhost>
2012-08-26 22:01:26 +0200

    Warning Cleanup

--- src/cat/atridas87/ld24/ai/RandomAI.java
+++ src/cat/atridas87/ld24/ai/RandomAI.java
@@ -3,7 +3,6 @@ package cat.atridas87.ld24.ai;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.Set;
