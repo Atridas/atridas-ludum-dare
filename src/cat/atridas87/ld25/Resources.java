677c84cd5ba95aa31110f353f47638dcb3bfac95
Atridas <Atridas@localhost>
2012-12-17 14:25:21 +0100

    realtime madness

---
+++ src/cat/atridas87/ld25/Resources.java
@@ -0,0 +1,285 @@
+package cat.atridas87.ld25;
+
+import java.util.ArrayList;
+import java.util.TreeSet;
+
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
+import cat.atridas87.ld25.modelData.Castle;
+import cat.atridas87.ld25.modelData.Sala;
+import cat.atridas87.ld25.modelData.Soul;
+import cat.atridas87.ld25.modelData.Castle.RoomSocket;
+import cat.atridas87.ld25.modelData.Castle.Point;
+import cat.atridas87.ld25.modelData.Wave;
+
+public abstract class Resources {
+
+	public static final String APP_NAME = "LD25";
+
+	public static final float DRAG_THRESHOLD = 10;
+	public static final float TIME_BETWEN_SOCKETS = 3;
+	public static final float TIME_ENTER = 2f;
+	public static final float TIME_CONSUMPTION = 10f;
+	public static final float WAVE_TIME = 4f;
+	public static final float ENTER_TIME = 0.5f;
+	
+	public static final float SPEED = 4;
+	
+
+	public static Castle createLevel0Castle() throws SlickException {
+
+		TreeSet<Sala> sales = new TreeSet<Sala>();
+		ArrayList<RoomSocket> sockets = new ArrayList<Castle.RoomSocket>();
+		ArrayList<Sala> salesInicials = new ArrayList<Sala>();
+
+		Sala aa = new Sala(new Image("resources/images/rooms/electricitat-petit.png"), 50,
+				Soul.A, Soul.A);
+		Sala bb = new Sala(new Image("resources/images/rooms/infectades-petit.png"), 50,
+				Soul.B, Soul.B);
+		Sala cc = new Sala(new Image("resources/images/rooms/parrilla-petit.png"), 50,
+				Soul.C, Soul.C);
+		Sala ab = new Sala(new Image("resources/images/rooms/ab.png"), 75,
+				Soul.A, Soul.B);
+		Sala ac = new Sala(new Image("resources/images/rooms/ac.png"), 75,
+				Soul.A, Soul.C);
+		Sala bc = new Sala(new Image("resources/images/rooms/bc.png"), 75,
+				Soul.B, Soul.C);
+		Sala abc = new Sala(new Image("resources/images/rooms/vampireses-petit.png"), 100,
+				Soul.A, Soul.B, Soul.C);
+		Sala aabbcc = new Sala(new Image("resources/images/rooms/gel-petit.png"),
+				250, Soul.A, Soul.A, Soul.B, Soul.B, Soul.C, Soul.C);
+
+		sales.add(aa);
+		sales.add(bb);
+		sales.add(cc);
+		sales.add(ab);
+		sales.add(ac);
+		sales.add(bc);
+		sales.add(abc);
+		sales.add(aabbcc);
+
+		salesInicials.add(aa);
+		sockets.add(new RoomSocket(42, 403, 144, 81));
+		salesInicials.add(bb);
+		sockets.add(new RoomSocket(221, 404, 144, 81));
+		salesInicials.add(cc);
+		sockets.add(new RoomSocket(378, 403, 144, 81));
+
+		salesInicials.add(null);
+		sockets.add(new RoomSocket(302, 257, 144, 81));
+		salesInicials.add(null);
+		sockets.add(new RoomSocket(107, 255, 144, 81));
+
+		// salesInicials.add(null);sockets.add(new RoomSocket(209, 132, 144,
+		// 81));
+
+		// -----
+
+		Point w0 = new Point(0, 500);
+		Point w1 = new Point(75, 500);
+		Point ew0 = new Point(75, 450);
+		Point w2 = new Point(275, 500);
+		Point ew1 = new Point(275, 450);
+		Point w3 = new Point(440, 500);
+		Point ew2 = new Point(440, 450);
+		Point w4 = new Point(510, 500);
+
+		Point w5 = new Point(510, 370);
+		Point w6 = new Point(360, 370);
+		Point ew3 = new Point(360, 320);
+		Point w7 = new Point(175, 370);
+		Point ew4 = new Point(175, 320);
+
+		ArrayList<ArrayList<Point>> waypoints = new ArrayList<ArrayList<Point>>();
+
+		ArrayList<Point> path0 = new ArrayList<Castle.Point>();
+		path0.add(w0);
+		path0.add(w1);
+
+		ArrayList<Point> path1 = new ArrayList<Castle.Point>();
+		path1.add(w1);
+		path1.add(w2);
+
+		ArrayList<Point> path2 = new ArrayList<Castle.Point>();
+		path2.add(w2);
+		path2.add(w3);
+
+		ArrayList<Point> path3 = new ArrayList<Castle.Point>();
+		path3.add(w3);
+		path3.add(w4);
+		path3.add(w5);
+		path3.add(w6);
+
+		ArrayList<Point> path4 = new ArrayList<Castle.Point>();
+		path4.add(w6);
+		path4.add(w7);
+
+		waypoints.add(path0);
+		waypoints.add(path1);
+		waypoints.add(path2);
+		waypoints.add(path3);
+		waypoints.add(path4);
+		
+		// -----
+
+		ArrayList<ArrayList<Point>> entryWaypoints = new ArrayList<ArrayList<Point>>();
+
+		ArrayList<Point> entryPath0 = new ArrayList<Castle.Point>();
+		entryPath0.add(w1);
+		entryPath0.add(ew0);
+
+		ArrayList<Point> entryPath1 = new ArrayList<Castle.Point>();
+		entryPath1.add(w2);
+		entryPath1.add(ew1);
+
+		ArrayList<Point> entryPath2 = new ArrayList<Castle.Point>();
+		entryPath2.add(w3);
+		entryPath2.add(ew2);
+
+		ArrayList<Point> entryPath3 = new ArrayList<Castle.Point>();
+		entryPath3.add(w6);
+		entryPath3.add(ew3);
+
+		ArrayList<Point> entryPath4 = new ArrayList<Castle.Point>();
+		entryPath4.add(w7);
+		entryPath4.add(ew4);
+
+		entryWaypoints.add(entryPath0);
+		entryWaypoints.add(entryPath1);
+		entryWaypoints.add(entryPath2);
+		entryWaypoints.add(entryPath3);
+		entryWaypoints.add(entryPath4);
+
+		// -----
+
+		return new Castle(540, 540, new Image("resources/images/level_0.png"),
+				sales, sockets, salesInicials, waypoints, entryWaypoints);
+	}
+
+	public static ArrayList<Wave> createLevel0Waves() {
+		ArrayList<Wave> waves = new ArrayList<Wave>();
+
+		waves.add(new Wave(0, 0, 0));
+		waves.add(new Wave(1, 0, 0));
+		waves.add(new Wave(0, 0, 0));
+		waves.add(new Wave(0, 1, 0));
+		waves.add(new Wave(0, 0, 0));
+		waves.add(new Wave(0, 0, 1));
+
+		waves.add(new Wave(0, 0, 0));
+		waves.add(new Wave(1, 1, 1));
+
+		waves.add(new Wave(0, 0, 0));
+		waves.add(new Wave(1, 0, 0));
+		waves.add(new Wave(0, 1, 0));
+		waves.add(new Wave(0, 0, 1));
+
+		waves.add(new Wave(0, 0, 0));
+		waves.add(new Wave(2, 0, 0));
+		waves.add(new Wave(0, 2, 0));
+		waves.add(new Wave(0, 0, 2));
+
+		waves.add(new Wave(0, 0, 0));
+		waves.add(new Wave(1, 1, 0));
+		waves.add(new Wave(0, 1, 1));
+		waves.add(new Wave(1, 0, 1));
+
+		waves.add(new Wave(0, 0, 0));
+		waves.add(new Wave(1, 1, 2));
+		waves.add(new Wave(2, 1, 1));
+		waves.add(new Wave(1, 2, 1));
+
+		// ----------------------
+
+		waves.add(new Wave(0, 0, 0));
+		waves.add(new Wave(0, 0, 0));
+		waves.add(new Wave(0, 0, 0));
+
+		waves.add(new Wave(3, 1, 2));
+		waves.add(new Wave(2, 3, 1));
+		waves.add(new Wave(1, 2, 3));
+		
+		waves.add(new Wave(0, 0, 0));
+
+		waves.add(new Wave(1, 3, 2));
+		waves.add(new Wave(2, 1, 3));
+		waves.add(new Wave(3, 2, 1));
+		
+		waves.add(new Wave(0, 0, 0));
+
+		waves.add(new Wave(4, 3, 2));
+		waves.add(new Wave(2, 4, 3));
+		waves.add(new Wave(3, 4, 3));
+		// ----------------------
+
+		waves.add(new Wave(0, 0, 0));
+		waves.add(new Wave(2, 2, 2));
+		waves.add(new Wave(4, 1, 1));
+		waves.add(new Wave(1, 1, 4));
+		waves.add(new Wave(0, 0, 0));
+
+		// ----------------------
+
+		waves.add(new Wave(3, 1, 0));
+		waves.add(new Wave(1, 1, 5));
+		waves.add(new Wave(0, 3, 1));
+		waves.add(new Wave(5, 1, 1));
+		waves.add(new Wave(1, 0, 3));
+		waves.add(new Wave(1, 5, 1));
+		waves.add(new Wave(0, 0, 0));
+		waves.add(new Wave(0, 0, 0));
+
+		// ----------------------
+
+		waves.add(new Wave(0, 0, 0));
+		waves.add(new Wave(0, 0, 0));
+		waves.add(new Wave(5, 1, 5));
+		waves.add(new Wave(1, 5, 5));
+		waves.add(new Wave(5, 5, 1));
+		waves.add(new Wave(2, 2, 2));
+		waves.add(new Wave(5, 5, 5));
+		waves.add(new Wave(1, 1, 1));
+		waves.add(new Wave(5, 1, 5));
+		waves.add(new Wave(1, 5, 5));
+		waves.add(new Wave(5, 5, 1));
+		waves.add(new Wave(2, 2, 2));
+		waves.add(new Wave(5, 5, 5));
+		waves.add(new Wave(1, 1, 1));
+		waves.add(new Wave(5, 1, 5));
+		waves.add(new Wave(1, 5, 5));
+		waves.add(new Wave(5, 5, 1));
+		waves.add(new Wave(2, 2, 2));
+		waves.add(new Wave(5, 5, 5));
+		waves.add(new Wave(1, 1, 1));
+		waves.add(new Wave(5, 1, 5));
+		waves.add(new Wave(1, 5, 5));
+		waves.add(new Wave(5, 5, 1));
+		waves.add(new Wave(2, 2, 2));
+		waves.add(new Wave(5, 5, 5));
+		waves.add(new Wave(1, 1, 1));
+		waves.add(new Wave(0, 0, 0));
+
+		return waves;
+	}
+
+	public static int pointCombo(int numSouls) {
+		if(numSouls < 10) {
+			return 10;
+		} else if(numSouls < 20) {
+			return 15;
+		} else if(numSouls < 30) {
+			return 25;
+		} else {
+			return 40;
+		}
+	}
+
+	public static int coinCombo(int numSouls) {
+		return 50;
+	}
+
+	public static enum State {
+		PLAYER_TURN
+	}
+}
---
+++ src/cat/atridas87/ld25/modelData/Level.java
@@ -0,0 +1,259 @@
+package cat.atridas87.ld25.modelData;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.newdawn.slick.UnicodeFont;
+
+import cat.atridas87.ld25.Resources;
+import cat.atridas87.ld25.modelData.Castle.RoomSocket;
+import cat.atridas87.ld25.render.FontManager;
+import cat.atridas87.ld25.render.ImageManager;
+
+public final class Level {
+	private Castle castle;
+	
+	private HashMap<Soul, Integer> reserve = new HashMap<Soul, Integer>();
+	private LinkedList<Wave> waves;
+
+	private float deltaToNextWave = 0;
+	private float deltaToNextSoul = 0;
+	private Soul lastEnteringSoul = Soul.C;
+	
+	private int soulsCombo = 0;
+	private int coins, points;
+	
+	//private boolean holdingMouse = false;
+	
+	public Level(Castle _castle, List<Wave> _waves, int initialCoins) {
+		castle = _castle;
+		coins = initialCoins;
+		points = 0;
+		waves = new LinkedList<Wave>(_waves);
+		
+		for(Soul soul : Soul.values()) {
+			reserve.put(soul, 0);
+		}
+	}
+
+	public void update(float ds) {
+		float waveDelta = ds / Resources.WAVE_TIME;
+		
+		deltaToNextWave += waveDelta;
+		
+		while(deltaToNextWave >= 1) {
+			deltaToNextWave -= 1;
+			
+			Wave wave = getNextWave();
+			
+			if(wave == null) {
+				levelComplete();
+				break;
+			}
+
+			addSoulsToReserve(Soul.A, wave.getSouls(Soul.A));
+			addSoulsToReserve(Soul.B, wave.getSouls(Soul.B));
+			addSoulsToReserve(Soul.C, wave.getSouls(Soul.C));
+		}
+		
+		float soulDelta = ds / Resources.ENTER_TIME;
+		
+		deltaToNextSoul += soulDelta;
+		while(deltaToNextSoul > 1) {
+			deltaToNextSoul -= 1;
+			
+			int cont = 0;
+			do {
+				lastEnteringSoul = lastEnteringSoul.next();
+				if(getReserve(lastEnteringSoul) > 0) {
+					getSoulsFromReserve(lastEnteringSoul, 1);
+					castle.addWalingSoul(lastEnteringSoul);
+					break;
+				}
+				cont++;
+			} while(cont < Soul.values().length);
+		}
+		
+		castle.update(ds);
+	}
+	
+	public Castle getCastle() {
+		return castle;
+	}
+	
+	public int getReserve(Soul soul) {
+		return reserve.get(soul);
+	}
+	
+	public void getSoulsFromReserve(Soul soul, int num) {
+		assert(num >= 0);
+		int res = reserve.get(soul);
+		res -= num;
+		assert(res >= 0);
+		reserve.put(soul, res);
+	}
+	
+	public void addSoulsToReserve(Soul soul, int num) {
+		assert(num >= 0);
+		int res = reserve.get(soul);
+		res += num;
+		assert(res >= 0);
+		reserve.put(soul, res);
+	}
+	
+	public void breakCombo() {
+		soulsCombo = 0;
+	}
+	
+	public void finishProcessingSoul() {
+		soulsCombo++;
+		addCoins(Resources.coinCombo(soulsCombo));
+		addPoints(Resources.pointCombo(soulsCombo));
+	}
+	
+	public void dropSoul(Soul soul) {
+		addSoulsToReserve(soul, 1);
+		breakCombo();
+	}
+	
+	private void levelComplete() {
+		// TODO
+	}
+	
+	public Wave getNextWave() {
+		if(waves.size() > 0) {
+			return waves.removeFirst();
+		} else {
+			return null;
+		}
+	}
+
+	public void scroll(float dy) {
+		castle.scroll(dy);
+	}
+	
+	/*
+	public void holdButton(boolean hold) {
+		holdingMouse = hold;
+	}
+	*/
+
+	public RoomSocket isSocket(float x, float y) {
+		return castle.isSocket(x, y);
+	}
+
+	public void setRoom(RoomSocket socket, Sala room) {
+		castle.setRoom(socket, room);
+	}
+	
+	public void takeCoins(int _coins) {
+		assert(_coins <= coins);
+		assert(_coins >= 0);
+		coins -= _coins;
+	}
+	
+	public void addCoins(int _coins) {
+		assert(_coins >= 0);
+		coins += _coins;
+	}
+	
+	public int getCoins() {
+		return coins;
+	}
+
+	public void addPoints(int _points) {
+		assert(_points >= 0);
+		points += _points;
+	}
+	
+	public int getPoints() {
+		return points;
+	}
+	
+	public Sala canGrabRoom(float x, float y) {
+		return castle.canGrabRoom(x, y);
+	}
+	
+	public void draw(float x, float y, float w, float h) {
+		ImageManager im = ImageManager.getInstance();
+
+		castle.drawCastle(x, y, 540 * w / 720, h);
+
+		
+		drawRightUI(im, 
+				540 * w / 720,
+				0,
+				180 * w / 720,
+				h);
+		
+	}
+	
+	private void drawRightUI(ImageManager im, float x, float y, float w, float h) {
+
+		
+		castle.drawConstructibleRooms(
+				x,
+				y + 130 * h / 540,
+				w,
+				410 * h / 540);
+		
+		// ------
+
+		float coinsDx = 10.f * w / 180;
+		float pointsDx = 100.f * w / 180;
+		float uiDy = 5.f * w / 180;
+		float uiSize = 17.f * w / 180;
+
+		float stringCoinsDx = 30.f * w / 180;
+		float stringPointsDx = 120.f * w / 180;
+		float stringDy = 5.f * w / 180;
+		UnicodeFont font = FontManager.getInstance().getFont((int)uiSize);
+
+		im.getCoinImage().draw(x + coinsDx, y + uiDy, uiSize, uiSize);
+		im.getPointsImage().draw(x + pointsDx, y + uiDy, uiSize, uiSize);
+
+		font.drawString(x + stringCoinsDx, y + stringDy, Integer.toString(coins));
+		font.drawString(x + stringPointsDx, y + stringDy, Integer.toString(points));
+				
+		// -------
+
+		float aDy = 32.f * w / 180;
+		float bDy = 62.f * w / 180;
+		float cDy = 92.f * w / 180;
+		
+		float iconDx = 10.f * w / 180;
+		float stringDx = 30.f * w / 180;
+
+		im.getSoulImage(Soul.A).draw(x + iconDx, aDy, uiSize, uiSize);
+		im.getSoulImage(Soul.B).draw(x + iconDx, bDy, uiSize, uiSize);
+		im.getSoulImage(Soul.C).draw(x + iconDx, cDy, uiSize, uiSize);
+		
+		font.drawString(x + stringDx, aDy, Integer.toString(getReserve(Soul.A)) + " / 10" );
+		font.drawString(x + stringDx, bDy, Integer.toString(getReserve(Soul.B)) + " / 10" );
+		font.drawString(x + stringDx, cDy, Integer.toString(getReserve(Soul.C)) + " / 10" );
+		
+		float waveDx = 80.f * w / 180;
+		float nextWaveDx = 30.f * w / 180;
+		
+		waveDx += nextWaveDx * (1 - deltaToNextWave);
+		
+		for(int i = 0; i < 4; i++) {
+			if(waves.size() <= i) break;
+			int a = waves.get(i).getSouls(Soul.A);
+			int b = waves.get(i).getSouls(Soul.B);
+			int c = waves.get(i).getSouls(Soul.C);
+			if(a > 0) {
+				im.getSoulImage(Soul.A, a).draw(x + waveDx, aDy, uiSize, uiSize);
+			}
+			if(b > 0) {
+				im.getSoulImage(Soul.B, b).draw(x + waveDx, bDy, uiSize, uiSize);
+			}
+			if(c > 0) {
+				im.getSoulImage(Soul.C, c).draw(x + waveDx, cDy, uiSize, uiSize);
+			}
+			
+			waveDx += nextWaveDx;
+		}
+	}
+}
