f28db68cdd1838dd84fa5934bb252125bd5d18ba
Atridas <Atridas@localhost>
2013-08-24 09:44:30 +0200

    textura de test funcionant.

--- Project/src/cat/atridas87/ld27/LD27.java
+++ Project/src/cat/atridas87/ld27/LD27.java
@@ -1,8 +1,18 @@
 package cat.atridas87.ld27;
 
+import java.awt.image.BufferedImage;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+
 import org.lwjgl.opengl.GL11;
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.util.BufferedImageUtil;
+import org.newdawn.slick.util.ResourceLoader;
 
 public class LD27 extends BaseGame {
+	
+	Texture texture;
 
 	public LD27(int _width, int _height) {
 		super(_width, _height);
@@ -17,8 +27,17 @@ public class LD27 extends BaseGame {
 	@Override
 	public void init() throws Exception {
 		// TODO Auto-generated method stub
+		
+		URL url = ResourceLoader.getResource("resources/Sin título.png");
+		BufferedImage bi = ImageIO.read(url);
+		
+		texture = BufferedImageUtil.getTexture("Sin título", bi);
 
+		System.out.println(url.toString());
+		System.out.println(bi.getWidth() + " x " + bi.getHeight());
+		
 		GL11.glClearColor(0, .5f, .5f, 1);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
 	}
 
 	@Override
@@ -26,6 +45,24 @@ public class LD27 extends BaseGame {
 
 		GL11.glClear(GL11.GL_DEPTH_BUFFER_BIT | GL11.GL_COLOR_BUFFER_BIT);
 
+		texture.bind();
+		
+		GL11.glBegin(GL11.GL_QUADS); {
+
+			GL11.glTexCoord2f(0, 1);
+			GL11.glVertex2f(-.5f, -.5f);
+			
+			GL11.glTexCoord2f(1, 1);
+			GL11.glVertex2f(+.5f, -.5f);
+			
+			GL11.glTexCoord2f(1, 0);
+			GL11.glVertex2f(+.5f, +.5f);
+			
+			GL11.glTexCoord2f(0, 0);
+			GL11.glVertex2f(-.5f, +.5f);
+		
+		} GL11.glEnd();
+		
 	}
 
 	@Override
