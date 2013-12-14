00a162b0972b039acad88ba7541b00f41f5aa091
Isaac 'Atridas' Serrano Guasch <atridas87@gmail.com>
2013-12-14 11:53:39 +0100

    projecte de unity

---
+++ UnityProj/Assets/PlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public KeyCode left, right, up, down;
+
+	public float speed;
+
+	private Quaternion initialRotation;
+
+	// Use this for initialization
+	void Start () {
+		initialRotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 velocity = rigidbody2D.velocity;
+		if (Input.GetKey (left)) {
+				velocity.x = -speed;
+		} else if (Input.GetKey (right)) {
+				velocity.x = speed;
+		} else {
+				velocity.x = 0;
+		}
+		rigidbody2D.velocity = velocity;
+		rigidbody2D.angularVelocity = 0;
+		transform.rotation = initialRotation;
+	}
+}
